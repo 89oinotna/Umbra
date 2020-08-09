b7aed659f737d8cbcf38d9f7678acf10bf2d3a30
89oinotna <a.z.zegarelli@live.it>
2020-08-09 21:17:28 +0200

    +added download

--- app/src/main/java/com/oinotna/umbra/ui/settings/SettingsFragment.java
+++ app/src/main/java/com/oinotna/umbra/ui/settings/SettingsFragment.java
@@ -1,7 +1,10 @@
 package com.oinotna.umbra.ui.settings;
 
+import android.content.Intent;
+import android.net.Uri;
 import android.os.Bundle;
 
+import androidx.preference.Preference;
 import androidx.preference.PreferenceFragmentCompat;
 
 import com.oinotna.umbra.R;
@@ -11,5 +14,11 @@ public class SettingsFragment extends PreferenceFragmentCompat {
     @Override
     public void onCreatePreferences(Bundle savedInstanceState, String rootKey) {
         setPreferencesFromResource(R.xml.root_preferences, rootKey);
+        Preference userButton = (Preference) findPreference("download");
+        userButton.setOnPreferenceClickListener(arg0 -> {
+            startActivity(new Intent(Intent.ACTION_VIEW,
+                    Uri.parse("https://github.com/89oinotna/UmbraServer/releases")));
+            return true;
+        });
     }
 }
